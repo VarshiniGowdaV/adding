cf911508422e7fdcd9db3c8127d75fac8eb915aa
VarshiniGowda.V <varshinigowdav14@gmail.com>
2024-10-15 15:07:29 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+    int r1,r2,r3,r4,r5;
+    void addition();
+    addition();
+}
+void addition()
+{
+    cout<<"insaid addition"<<endl;
+    int a1=6;
+    int a2=8;
+    int a3=2;
+    int a4=2;
+    int a5=3;
+    int a6=3;
+    int a7=4;
+    int a8=4;
+    int a9=5;
+    int a10=5;
+    int r1;
+    int r2;
+    int r3;
+    int r4;
+    int r5;
+    r1=a1+a2;
+    r2=a3+a4;
+    r3=a5+a6;
+    r4=a7+a8;
+    r5=a9+a10;
+    cout<<"result of r1"<< r1<<endl;
+    cout<<"result of r2"<< r2<<endl;
+    cout<<"result of r3"<< r3<<endl;
+    cout<<"result of r4"<< r4<<endl;
+    cout<<"result of r5"<< r5<<endl;
+}
